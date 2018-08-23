812bd888af9a5c731413144921d9c3f56bc4eb93
celestino.salim@gmail.com <celestino.salim@gmail.com>
2018-08-23 01:02:49 +0000

    Done.

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,13 +1,14 @@
 def my_hash
-  intro = {"key" => "value"}
-
-
+  valid_hash = { "key" => "value" }
 end
 
 
 def shipping_manifest
- the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
+  the_manifest = { "whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3 }
+
 end
+
+
 
 def retrieval
   shipping_manifest = {
@@ -16,7 +17,8 @@ def retrieval
                   "oil paintings" => 3
                  }
 
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
+
+#   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
 shipping_manifest["oil paintings"]
 end
 
@@ -26,15 +28,13 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
+# add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
 
 
-  # add 4 gun powder to the shipping_manifest hash below
-  shipping_manifest["gun powder"] = 4
+# add 4 gun powder to the shipping_manifest hash below
+shipping_manifest["gun powder"] = 4
 
-
-
-  # return the shipping_manifest hash below
+# return the shipping_manifest hash below
 shipping_manifest
 end
